25aef98b9445e06cf1fb8c4bd52a7130ed2cd826
Shashank Tiwari <tiwarishashank325@gmail.com>
2021-09-18 08:49:13 +0530

    goroutines

--- ChannelsAndGoroutines/main.go
+++ ChannelsAndGoroutines/main.go
@@ -1,20 +1,15 @@
 package main
 import "fmt"
 
-func  add (a int, b int) int {
-	return a+b
-}
-
 func main() {
 
-	// finish := make(chan bool) // Unbuffered Channel
-	// //	buffered :=make(chan bool, 2) //Buffered channel of size 2
-	// go func() {  // goroutine
-	// 	fmt.Println("Hi")
-	// 	finish <- true
-	// }()
-	// fmt.Println("There")
-	// <-finish 
-	fmt.Println(add(2,4))
+	finish := make(chan bool, 2) // Unbuffered Channel
+	//	buffered :=make(chan bool, 2) //Buffered channel of size 2
+	go func() {
+		fmt.Println("Hi")
+		finish <- true
+	}()
+	fmt.Println("There")
+	<-finish
 
 }
