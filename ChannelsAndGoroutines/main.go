0606aceedbe9e30db67d603a10764b1dc3f94c8c
Shashank Tiwari <tiwarishashank325@gmail.com>
2021-09-18 08:48:17 +0530

    Rollback

--- ChannelsAndGoroutines/main.go
+++ ChannelsAndGoroutines/main.go
@@ -1,15 +1,20 @@
 package main
 import "fmt"
 
+func  add (a int, b int) int {
+	return a+b
+}
+
 func main() {
 
-	finish := make(chan bool) // Unbuffered Channel
-	//	buffered :=make(chan bool, 2) //Buffered channel of size 2
-	go func() {  // goroutine
-		fmt.Println("Hi")
-		finish <- true
-	}()
-	fmt.Println("There")
-	<-finish 
+	// finish := make(chan bool) // Unbuffered Channel
+	// //	buffered :=make(chan bool, 2) //Buffered channel of size 2
+	// go func() {  // goroutine
+	// 	fmt.Println("Hi")
+	// 	finish <- true
+	// }()
+	// fmt.Println("There")
+	// <-finish 
+	fmt.Println(add(2,4))
 
 }
