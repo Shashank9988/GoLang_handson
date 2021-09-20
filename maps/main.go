f35ac1dd52c2db56c18b82bb766c4f210a0b5a14
Shashank Tiwari <tiwarishashank325@gmail.com>
2021-09-20 08:27:55 +0530

    map

--- maps/main.go
+++ maps/main.go
@@ -3,24 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// Define map
-	// emails := make(map[string]string)
+	
+	emails := map[string]string{"Shashank": "tiwarishashank325@gmail.com", "Rahuk": "rahul@gmail.com"}
 
-	//  Assign kv
-	// emails["Bob"] = "bob@gmail.com"
-	// emails["Sharon"] = "sharon@gmail.com"
-	// emails["Mike"] = "mike@gmail.com"
-
-	// Decalre map and add kv
-	emails := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com"}
-
-	emails["Mike"] = "mike@gmail.com"
+	emails["James"] = "james@gmail.com"
 
 	fmt.Println(emails)
 	fmt.Println(len(emails))
-	fmt.Println(emails["Bob"])
+	fmt.Println(emails["James"])
 
 	// Delete from map
-	delete(emails, "Bob")
+	delete(emails, "James")
 	fmt.Println(emails)
+
+	// MAP using make
+	mp := make(map[string]int)
+	
+
+
 }
